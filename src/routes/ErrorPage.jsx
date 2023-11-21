4404123836b1079397c5e1e0a56eb49a54ea92ff
soyeon <soylee57@naver.com>
2023-11-21 16:03:22 +0900

    Error Page - Styling

--- src/routes/ErrorPage.jsx
+++ src/routes/ErrorPage.jsx
@@ -1,15 +1,38 @@
-import React from 'react';
+import React, { useMemo, useState } from 'react';
 import { useRouteError } from "react-router-dom";
 
 export default function ErrorPage() {
   const error = useRouteError();
-  console.error(error);
+  useMemo(() => {console.error(error)}, [])
+
+  const [position, setPosition] = useState({x: 0, y: 0});
+  const moveHandler = (e) => {
+    const speed = 2;
+    setPosition({
+      x: (window.innerWidth - e.clientX * speed) / 100,
+      y: (window.innerHeight - e.clientY * speed) / 100,
+    })
+  }
+
 
   return (
-    <div>
-      <h1>🤔 Sorry, an unexpected error has occured.</h1>
-      <p>{error.statusText || error.message}</p>
-    </div>
+    <section 
+      className='p-4 w-full h-screen flex flex-col items-center justify-center text-center'
+      onMouseMove={moveHandler}
+    >
+      <img 
+        className='w-60 md:w-auto'
+        style={{transform: `translate(${position.x}px, ${position.y}px)`, transitionTimingFunction: 'ease'}}
+        src='/images/errorIcon.png' 
+        alt='error' 
+      />
+      <h1 className='text-4xl font-bold mt-12'>
+          {`${error.status} - ${error.statusText}` || error.message}
+      </h1>
+      <p className='text-xl md:text-2xl font-semibold mt-4'>
+        Sorry, an unexpected error has occured.
+      </p>
+    </section>
   );
 }
 
