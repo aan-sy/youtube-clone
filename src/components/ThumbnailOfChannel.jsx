f38cd0d997e7302f1b9cbc508dfea6922ff0b75d
soyeon <soylee57@naver.com>
2023-12-06 13:37:47 +0900

    ThumbnailOfChannel 코드 수정

--- src/components/ThumbnailOfChannel.jsx
+++ src/components/ThumbnailOfChannel.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 
-export default function ThumbnailOfChannel({ id, title }) {
+export default function ThumbnailOfChannel({ channelId }) {
   const {isLoading, error, data: thumbnailUrl} = useQuery({
-    queryKey: ['thumbnail'],
+    queryKey: ['thumbnail', channelId],
     queryFn: async () => {
-      console.log('fetch thumbnail..');
-      // `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${id}&key=${key}`
-      return axios({ method: 'get', url: '/data/channel.json'}).then(res => res.data.items[0].snippet.thumbnails.default.url)
+      // const url = `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&id=${channelId}&key=`;
+      const url = '/data/channel.json'; 
+      return axios({ method: 'get', url}).then(res => res.data.items[0].snippet.thumbnails.default.url)
     },
     staleTime: 1000 * 60 * 5,
   })
@@ -22,7 +22,7 @@ export default function ThumbnailOfChannel({ id, title }) {
 
   return (
     <>
-      {thumbnailUrl && <img className='shrink-0 mr-2 object-contain w-12 h-12 rounded-full' src={thumbnailUrl} alt={title} />}      
+      {thumbnailUrl && <img className='shrink-0 mr-2 object-contain w-12 h-12 rounded-full' src={thumbnailUrl} alt='' />}      
     </>
   );
 }
